ed7f0c696106f813b4ab7abaccecacaa8f0d3285
Hyra <stef@mindthecode.com>
2017-02-26 17:18:39 +0100

    ProxyTable example

--- template/config/index.js
+++ template/config/index.js
@@ -27,7 +27,17 @@ module.exports = {
         autoOpenBrowser: true,
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
-        proxyTable: {},
+        proxyTable: {
+            // proxy all requests starting with /api to jsonplaceholder
+            // Example:
+            // '/api': {
+            //     target: 'http://jsonplaceholder.typicode.com',
+            //     changeOrigin: true,
+            //     pathRewrite: {
+            //         '^/api': ''
+            //     }
+            // }
+        },
         // CSS Sourcemaps off by default because relative paths are "buggy"
         // with this option, according to the CSS-Loader README
         // (https://github.com/webpack/css-loader#sourcemaps)
